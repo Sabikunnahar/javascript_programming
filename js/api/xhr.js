073494aa6f1879b882fa31f79e61d82e80ce2570
Sabikun Nahar Bristy <30294762+Sabikunnahar@users.noreply.github.com>
2025-03-02 16:12:26 +0600

    Core: API calling through XMLHttpRequest
    Core: API calling through XMLHttpRequest

---
+++ js/api/xhr.js
@@ -0,0 +1,16 @@
+//XMLHttpRequest - This is an old way to get data from old browsers. It needs to be known because some old browsers still use it. and Axios is developed based on XMLHttpRequest.
+
+/* What we will learn:
+    - event - onload, onerror
+    - property- response, responseText, status, readyState, statusText, responseType
+    - function - open(), send(), setRequestHeader()
+
+*/
+
+
+const getData =() =>{
+    const xhr = new XMLHttpRequest();
+    
+
+}
+getData();
